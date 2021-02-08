f6756580ba7769776fc8cffd9a2718e5e5dd2ad2
Ole <refsa22@gmail.com>
2021-02-08 04:24:23 +0100

    feat: il generator for ValueTuple

---
+++ RePacker/Generator/ValueTupleGenerator.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using Refsa.RePacker.Buffers;
+using Refsa.RePacker.Utils;
+using Buffer = Refsa.RePacker.Buffers.Buffer;
+
+namespace Refsa.RePacker.Generator
+{
+    public class ValueTupleGenerator : IGenerator
+    {
+        public GeneratorType GeneratorType => GeneratorType.Object;
+        public Type ForType => typeof(ITuple);
+
+        public void GenerateDeserializer(ILGenerator ilGen, FieldInfo fieldInfo)
+        {
+            var genArgs = fieldInfo.FieldType.GetGenericArguments();
+
+            var meth = typeof(PackerExtensions)
+                .GetMethods(BindingFlags.Public | BindingFlags.Static)
+                .Where(e => 
+                    e.Name.Contains(nameof(PackerExtensions.UnpackValueTuple)) &&
+                    e.GetGenericArguments().Length == genArgs.Length
+                ).First()
+                .MakeGenericMethod(genArgs);
+
+            ilGen.EmitCall(OpCodes.Call, meth, Type.EmptyTypes);
+        }
+
+        public void GenerateSerializer(ILGenerator ilGen, FieldInfo fieldInfo)
+        {
+            var genArgs = fieldInfo.FieldType.GetGenericArguments();
+
+            var meth = typeof(PackerExtensions)
+                .GetMethods(BindingFlags.Public | BindingFlags.Static)
+                .Where(e => 
+                    e.Name.Contains(nameof(PackerExtensions.PackValueTuple)) &&
+                    e.GetGenericArguments().Length == genArgs.Length
+                ).First()
+                .MakeGenericMethod(genArgs);
+
+            ilGen.EmitCall(OpCodes.Call, meth, Type.EmptyTypes);
+        }
+    }
+}
