20e0ff86c72cf51b78cce4fa7b99c12c90c64d07
Ole <refsa22@gmail.com>
2021-02-08 04:24:15 +0100

    feat: il generator for KeyValuePair

---
+++ RePacker/Generator/KeyValuePairGenerator.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using Refsa.RePacker.Buffers;
+using Refsa.RePacker.Utils;
+using Buffer = Refsa.RePacker.Buffers.Buffer;
+
+namespace Refsa.RePacker.Generator
+{
+    public class KeyValuePairGenerator : IGenerator
+    {
+        public GeneratorType GeneratorType => GeneratorType.Object;
+        public Type ForType => typeof(KeyValuePair<,>);
+
+        public void GenerateDeserializer(ILGenerator ilGen, FieldInfo fieldInfo)
+        {
+            var genArgs = fieldInfo.FieldType.GetGenericArguments();
+
+            Console.WriteLine(genArgs[0] + " - " + genArgs[1]);
+
+            var unpackMethod = typeof(PackerExtensions)
+                .GetMethod(nameof(PackerExtensions.UnpackKeyValuePair))
+                .MakeGenericMethod(genArgs);
+
+            ilGen.EmitCall(OpCodes.Call, unpackMethod, Type.EmptyTypes);
+        }
+
+        public void GenerateSerializer(ILGenerator ilGen, FieldInfo fieldInfo)
+        {
+            var genArgs = fieldInfo.FieldType.GetGenericArguments();
+            var packMethod = typeof(PackerExtensions)
+                .GetMethod(nameof(PackerExtensions.PackKeyValuePair))
+                .MakeGenericMethod(genArgs);
+
+            ilGen.EmitCall(OpCodes.Call, packMethod, Type.EmptyTypes);
+        }
+    }
+}
