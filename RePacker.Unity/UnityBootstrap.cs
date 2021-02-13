bcc6b57e6572822fdc7f135bdf83d4c74eb22745
Ole <refsa22@gmail.com>
2021-02-13 10:44:42 +0100

    unity: compiler defines to turn off bootstrap and logging

--- RePacker.Unity/UnityBootstrap.cs
+++ RePacker.Unity/UnityBootstrap.cs
@@ -3,15 +3,21 @@ using Refsa.RePacker.Utils;
 
 namespace Refsa.RePacker.Unity
 {
+#if !NO_BOOTSTRAP
     public static class UnityBootstrap
     {
         static UnityBootstrap()
         {
             var unitySettings = new RePackerSettings(new UnityLogger());
+#if DISABLE_LOGGING
+                unitySettings.LoggingEnabled = false;
+#endif
+
             RePacker.Init(unitySettings);
         }
 
         [UnityEngine.RuntimeInitializeOnLoadMethod]
         static void SetupRePacker() { }
     }
+#endif
 }
