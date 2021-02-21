01fa832c95da859648a6d9c201d97122ed64db55
Ole <refsa22@gmail.com>
2021-02-21 12:51:22 +0100

    refactor: packing Dictionary as KeyValuePair IEnumerable instead of two seperate lists for Key and Value

--- RePacker/Packer/CustomPackers/Collections/DictionaryPacker.cs
+++ RePacker/Packer/CustomPackers/Collections/DictionaryPacker.cs
@@ -9,21 +9,21 @@ namespace RePacker.Builder
     {
         public override void Pack(BoxedBuffer buffer, ref Dictionary<TKey, TValue> value)
         {
-            buffer.PackIEnumerable(value.Keys);
-            buffer.PackIEnumerable(value.Values);
-            
-            // var kvs = value.AsEnumerable();
-            // buffer.Pack(ref kvs);
+            // buffer.PackIEnumerable(value.Keys);
+            // buffer.PackIEnumerable(value.Values);
+
+            var kvs = value.AsEnumerable();
+            buffer.PackIEnumerable(kvs);
         }
 
         public override void Unpack(BoxedBuffer buffer, out Dictionary<TKey, TValue> value)
         {
-            buffer.UnpackIEnumerable<TKey>(out var keys);
-            buffer.UnpackIEnumerable<TValue>(out var values);
-            value = keys.Zip(values, (k, v) => new KeyValuePair<TKey, TValue>(k, v)).ToDictionary(x => x.Key, x => x.Value);
+            // buffer.UnpackIEnumerable<TKey>(out var keys);
+            // buffer.UnpackIEnumerable<TValue>(out var values);
+            // value = keys.Zip(values, (k, v) => new KeyValuePair<TKey, TValue>(k, v)).ToDictionary(x => x.Key, x => x.Value);
 
-            // var kvCollection = buffer.Unpack<IEnumerable<KeyValuePair<TKey, TValue>>>();
-            // value = kvCollection.ToDictionary(x => x.Key, x => x.Value);
+            buffer.UnpackIEnumerable(out IEnumerable<KeyValuePair<TKey, TValue>> kvCollection);
+            value = kvCollection.ToDictionary(x => x.Key, x => x.Value);
         }
     }
 }
