92901efb73ed477f0e75d6a9adf01b770a47d542
Ole <refsa22@gmail.com>
2021-02-23 16:35:02 +0100

    chore: proper nuget preparation

--- prepare_nuget.bat
+++ prepare_nuget.bat
@@ -1,3 +1,4 @@
-dotnet pack -o ../nuget/ RePacker/RePacker.csproj -c Release
-dotnet pack -o ../nuget/ RePacker.Unsafe/RePacker.Unsafe.csproj -c Release
+dotnet pack -o ../nuget/ -p:NuspecFile=RePacker.nuspec RePacker/RePacker.csproj -c Release
+
+dotnet pack -o ../nuget/ -p:NuspecFile=RePacker.Unsafe.nuspec RePacker.Unsafe/RePacker.Unsafe.csproj -c Release
 
