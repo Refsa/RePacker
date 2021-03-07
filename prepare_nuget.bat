9576aae7ae5f3656d4f9bc666c1e8e5c2f731de2
Ole <refsa22@gmail.com>
2021-03-07 10:07:33 +0100

    chore: fixed some nuget packing stuff

--- prepare_nuget.bat
+++ prepare_nuget.bat
@@ -1,4 +1,10 @@
-dotnet pack -o ../nuget/ -p:NuspecFile=RePacker.nuspec RePacker/RePacker.csproj -c Release
+rm -r RePacker/bin/
+rm -r RePacker/obj/
+dotnet restore RePacker/RePacker.csproj
+dotnet pack RePacker/RePacker.csproj -c Release -o ../nuget/
 
-dotnet pack -o ../nuget/ -p:NuspecFile=RePacker.Unsafe.nuspec RePacker.Unsafe/RePacker.Unsafe.csproj -c Release
+rm -r RePacker.Unsafe/bin/
+rm -r RePacker.Unsafe/obj/
+dotnet restore RePacker.Unsafe/RePacker.Unsafe.csproj
+dotnet pack RePacker.Unsafe/RePacker.Unsafe.csproj -c Release -o ../nuget/ -p:License=MIT
 
