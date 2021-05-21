7f4f3c5ced079565e8eb9de87e7e286490377efb
Ole <refsa22@gmail.com>
2021-05-21 07:54:54 +0200

    test: test cases for Unity types using RePacking.Pack

--- RePacker.Unity.Tests/Assets/Tests/UnityBufferPackTests.cs
+++ RePacker.Unity.Tests/Assets/Tests/UnityBufferPackTests.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
@@ -9,166 +9,166 @@ using RePacker.Unity;
 
 namespace Tests
 {
-    public class UnityWrapperTests
+    public class UnityBufferPackTests
     {
         ReBuffer buffer = new ReBuffer(1024);
 
         [Test]
-        public void wrapper_quaternion_packing()
+        public void direct_quaternion_packing()
         {
             buffer.Reset();
 
             Quaternion quat = Quaternion.AngleAxis(45f, Vector3.right);
-            RePacking.Pack(buffer, ref quat);
+            buffer.Pack(ref quat);
 
-            Quaternion from = RePacking.Unpack<Quaternion>(buffer);
+            buffer.Unpack(out Quaternion from);
             Assert.AreEqual(quat, from);
             Assert.AreEqual(quat.eulerAngles, from.eulerAngles);
         }
 
         [Test]
-        public void wrapper_vector2_packing()
+        public void direct_vector2_packing()
         {
             buffer.Reset();
 
             Vector2 vec = new Vector2(1.2f, 2.323423f);
-            RePacking.Pack(buffer, ref vec);
+            buffer.Pack(ref vec);
 
-            Vector2 from = RePacking.Unpack<Vector2>(buffer);
+            buffer.Unpack(out Vector2 from);
             Assert.AreEqual(vec, from);
         }
 
         [Test]
-        public void wrapper_vector3_packing()
+        public void direct_vector3_packing()
         {
             buffer.Reset();
 
             Vector3 vec = new Vector3(1.2f, 2.3f, 4.345235235f);
-            RePacking.Pack(buffer, ref vec);
+            buffer.Pack(ref vec);
 
-            Vector3 from = RePacking.Unpack<Vector3>(buffer);
+            buffer.Unpack(out Vector3 from);
             Assert.AreEqual(vec, from);
         }
 
         [Test]
-        public void wrapper_vector4_packing()
+        public void direct_vector4_packing()
         {
             buffer.Reset();
 
             Vector4 vec = new Vector4(1.2f, 2.3f, 4.345235235f, 342.1234754f);
-            RePacking.Pack(buffer, ref vec);
+            buffer.Pack(ref vec);
 
-            Vector4 from = RePacking.Unpack<Vector4>(buffer);
+            buffer.Unpack(out Vector4 from);
             Assert.AreEqual(vec, from);
         }
 
         [Test]
-        public void wrapper_vector2int_packing()
+        public void direct_vector2int_packing()
         {
             buffer.Reset();
 
             Vector2Int vec = new Vector2Int(123, 34262345);
-            RePacking.Pack(buffer, ref vec);
+            buffer.Pack(ref vec);
 
-            Vector2Int from = RePacking.Unpack<Vector2Int>(buffer);
+            buffer.Unpack(out Vector2Int from);
             Assert.AreEqual(vec, from);
         }
 
         [Test]
-        public void wrapper_vector3int_packing()
+        public void direct_vector3int_packing()
         {
             buffer.Reset();
 
             Vector3Int vec = new Vector3Int(123, 34262345, 934562345);
-            RePacking.Pack(buffer, ref vec);
+            buffer.Pack(ref vec);
 
-            Vector3Int from = RePacking.Unpack<Vector3Int>(buffer);
+            buffer.Unpack(out Vector3Int from);
             Assert.AreEqual(vec, from);
         }
 
         [Test]
-        public void wrapper_matrix4x4_packing()
+        public void direct_matrix4x4_packing()
         {
             buffer.Reset();
 
             Matrix4x4 mat = Matrix4x4.TRS(Vector3.one * 12345, Quaternion.AngleAxis(45f, Vector3.right), Vector3.one * 23);
-            RePacking.Pack(buffer, ref mat);
+            buffer.Pack(ref mat);
 
-            Matrix4x4 from = RePacking.Unpack<Matrix4x4>(buffer);
+            buffer.Unpack(out Matrix4x4 from);
 
             Assert.AreEqual(mat, from);
         }
 
         [Test]
-        public void wrapper_color_packing()
+        public void direct_color_packing()
         {
             buffer.Reset();
 
             Color color = new Color(0.2f, 0.3f, 0.4f, 0.5f);
-            RePacking.Pack(buffer, ref color);
+            buffer.Pack(ref color);
 
-            Color from = RePacking.Unpack<Color>(buffer);
+            buffer.Unpack(out Color from);
 
             Assert.AreEqual(color, from);
         }
 
         [Test]
-        public void wrapper_color32_packing()
+        public void direct_color32_packing()
         {
             buffer.Reset();
 
             Color32 color = new Color32(10, 50, 122, 240);
-            RePacking.Pack(buffer, ref color);
+            buffer.Pack(ref color);
 
-            Color32 from = RePacking.Unpack<Color32>(buffer);
+            buffer.Unpack(out Color32 from);
             Assert.AreEqual(color, from);
         }
 
         [Test]
-        public void wrapper_rectInt_packing()
+        public void direct_rectInt_packing()
         {
             buffer.Reset();
 
             RectInt rect = new RectInt(10, 20, 255, 562436);
-            RePacking.Pack(buffer, ref rect);
+            buffer.Pack(ref rect);
 
-            RectInt from = RePacking.Unpack<RectInt>(buffer);
+            buffer.Unpack(out RectInt from);
             Assert.AreEqual(rect, from);
         }
 
         [Test]
-        public void wrapper_rect_packing()
+        public void direct_rect_packing()
         {
             buffer.Reset();
 
             Rect rect = new Rect(10.23f, 20.3453f, 255.345346f, 562436.54345f);
-            RePacking.Pack(buffer, ref rect);
+            buffer.Pack(ref rect);
 
-            Rect from = RePacking.Unpack<Rect>(buffer);
+            buffer.Unpack(out Rect from);
             Assert.AreEqual(rect, from);
         }
 
         [Test]
-        public void wrapper_bounds_packing()
+        public void direct_bounds_packing()
         {
             buffer.Reset();
 
             Bounds bounds = new Bounds(Vector3.one * 325.234f, Vector3.one * 5634.345f);
-            RePacking.Pack(buffer, ref bounds);
+            buffer.Pack(ref bounds);
 
-            Bounds from = RePacking.Unpack<Bounds>(buffer);
+            buffer.Unpack(out Bounds from);
             Assert.AreEqual(bounds, from);
         }
 
         [Test]
-        public void wrapper_boundsInt_packing()
+        public void direct_boundsInt_packing()
         {
             buffer.Reset();
 
             BoundsInt bounds = new BoundsInt(Vector3Int.one * 325, Vector3Int.one * 5634);
-            RePacking.Pack(buffer, ref bounds);
+            buffer.Pack(ref bounds);
 
-            BoundsInt from = RePacking.Unpack<BoundsInt>(buffer);
+            buffer.Unpack(out BoundsInt from);
             Assert.AreEqual(bounds, from);
         }
     }
