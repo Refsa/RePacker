dca3a78736dba00393ef6431135fb41ae304ad20
Ole <refsa22@gmail.com>
2021-05-21 04:45:26 +0200

    test: added Unity tests project

---
+++ RePacker.Unity.Tests/UnityWrapperTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace RePacker.Unity.Tests
+{
+    public class UnityWrapperTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
