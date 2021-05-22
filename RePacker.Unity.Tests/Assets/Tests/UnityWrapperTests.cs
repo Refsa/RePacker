7e277f9c20d981626be0a16f83bbdd17260e07c4
Ole <refsa22@gmail.com>
2021-05-22 08:06:47 +0200

    test: removing failing unity test case chore: only running unity workflow when pushing to unity folder

--- RePacker.Unity.Tests/Assets/Tests/UnityWrapperTests.cs
+++ RePacker.Unity.Tests/Assets/Tests/UnityWrapperTests.cs
@@ -14,12 +14,6 @@ namespace Tests
         ReBuffer buffer = new ReBuffer(1024);
 
         [Test]
-        public void ThisWillFail()
-        {
-            Assert.True(false);
-        }
-
-        [Test]
         public void wrapper_quaternion_packing()
         {
             buffer.Reset();
